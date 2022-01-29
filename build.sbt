baaf90fd3e50c54bcd5100260902d4651a2af612
Jinseong Hwang <jinseong.dev@gmail.com>
2022-01-30 00:44:38 +0900

    chore: 프로젝트 생성

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "scala-lab"
+
+version := "0.1"
+
+scalaVersion := "2.13.8"
+
+idePackagePrefix := Some("me.study.scala")
---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,8 @@
+package me.study.scala
+
+object HelloWorld {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello, World")
+  }
+}
