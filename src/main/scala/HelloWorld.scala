e6e9be4443fdc489cbcb37b94a1167756e912793
Jinseong Hwang <jinseong.dev@gmail.com>
2022-01-31 23:34:40 +0900

    docs: 코드 설명 주석 추가

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,7 +1,15 @@
 package me.study.scala
 
+/**
+ * object는 싱글턴 객체를 선언과 동시에 생성하는 것이다.
+ */
 object HelloWorld {
 
+  /**
+   * Java와 달리 main 메서드가 static이 아니다.
+   * Scala에서는 static 이 존재하지 않는다.
+   * static이길 기대하는 멤버들은 싱글턴 객체 안에 선언한다.
+   */
   def main(args: Array[String]): Unit = {
     println("Hello, World")
   }
