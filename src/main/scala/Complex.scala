4e8a6700d64623715e7e17f4b3b7a789e26176e1
Jinseong Hwang <jinseong.dev@gmail.com>
2022-02-01 00:55:49 +0900

    feat: Scala의 클래스

---
+++ src/main/scala/Complex.scala
@@ -0,0 +1,11 @@
+package me.study.scala
+
+/**
+ * Scala 에는 클래스에 인자를 가질 수 있다.
+ * 생성자 같은 느낌으로 봐도 좋겠다.
+ */
+class Complex(real: Double, imaginary: Double) {
+  // 아래 메서드에는 반환 타입을 생략했는데, 추측 가능하기 때문이다.
+  def re() = real
+  def im() = imaginary
+}
