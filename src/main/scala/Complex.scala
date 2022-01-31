ed0785d4f5120dfa810420e9f2cb6f40df05e882
Jinseong Hwang <jinseong.dev@gmail.com>
2022-02-01 01:03:25 +0900

    feat: 오버라이딩

--- src/main/scala/Complex.scala
+++ src/main/scala/Complex.scala
@@ -9,4 +9,8 @@ class Complex(real: Double, imaginary: Double) {
   // 메서드이지만, () 까지 생략할 수 있다. getter 같은 느낌인가?
   def re = real
   def im = imaginary
+
+  // Scala의 모든 클래스는 scala.AnyRef 를 상속받는다. (마치 Java에서 Object를 상속받듯)
+  // 따라서 toString 을 오버라이딩 할 수 있는데, 반드시 override 키워드를 붙여줘야 한다.
+  override def toString: String = "" + re + (if (im >= 0) "+" else "") + im + "i"
 }
--- src/main/scala/ComplexNumbers.scala
+++ src/main/scala/ComplexNumbers.scala
@@ -5,5 +5,6 @@ object ComplexNumbers {
   def main(args: Array[String]): Unit = {
     val c = new Complex(1.2, 3.4)
     println("imaginary part : " + c.im) // 클래스 멤버 선언 시 ()를 생략했다면, 사용할 때도 빼자.
+    println(c) // toString 을 재정의했기 때문
   }
 }
